edfaf79cf362d2a78e3bd6a083d9b39b8a491b19
joh11155 <johannes.gruendler@fokus.fraunhofer.de>
2023-06-07 15:18:11 +0200

    First sketch of the task

---
+++ Issue/Issue.py
@@ -0,0 +1,9 @@
+class Issue:
+    def __init__(self):
+        self.body = None
+        self.tags = None
+        self.dateRaised = None
+        self.dateSolved = None
+        self.answer = None
+        self.firstAnswer = None
+        self.avrgAnswerTime = None
